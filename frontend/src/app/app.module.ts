5a1098a46abe0f07268acc1722bd6a86c651ffb6
Lucas Peixoto <lucas.sacramoni@yahoo.com>
2021-04-16 08:25:42 -0300

    Estruturação inicial do componente home

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -13,14 +13,18 @@ import { NavComponent } from './components/template/nav/nav.component'
 
 //Módulos para criação do componente de Navegação
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list'
+import { MatListModule } from '@angular/material/list';
+import { HomeComponent } from './views/home/home.component'
+
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -28,7 +32,8 @@ import { MatListModule } from '@angular/material/list'
     BrowserAnimationsModule,
     MatToolbarModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
