1ddbf030ee1daa6081db85f2b609f0c4da7fd7ab
Lucas Peixoto <lucas.sacramoni@yahoo.com>
2021-04-18 14:04:36 -0300

    Inclusão da coluna de ações

--- frontend/src/app/components/product/product-read/product-read.component.ts
+++ frontend/src/app/components/product/product-read/product-read.component.ts
@@ -12,18 +12,13 @@ export class ProductReadComponent implements OnInit {
   products!: Product[]
 
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
-  displayedColumns = ['id', 'name', 'price', 'storage'];
+  displayedColumns = ['id', 'name', 'price', 'storage', 'action'];
 
   constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
     this.productService.read().subscribe(products => {
       this.products = products
-
-      //Setar produtos na tabela
-
-
     })
   }
-
 }
